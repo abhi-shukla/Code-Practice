9c9fdde9731fec8716144fdf202f4efa5bb3dc8b
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-12 00:22:56 -0700

    Add String Replacement Solution

---
+++ StringReplacement/StringReplacement.Tests/ReplacerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringReplacement;
+
+namespace StringReplacement.Tests
+{
+    [TestClass]
+    public class ReplacerTests
+    {
+        [TestMethod]
+        public void IsReplaceable_OnReplaceableString_ShouldReturnTrue()
+        {
+            // Arrange
+            var replacer = new Replacer();
+
+            // Act
+            var res = replacer.IsReplaceable("pale", "bale");
+
+            // Assert
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsReplaceable_OnEditableString_ShouldReturnTrue()
+        {
+            // Arrange
+            var replacer = new Replacer();
+
+            // Act
+            var res = replacer.IsReplaceable("pale", "pales");
+
+            // Assert
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsReplaceable_OnNonReplaceableString_ShouldReturnTrue()
+        {
+            // Arrange
+            var replacer = new Replacer();
+
+            // Act
+            var res = replacer.IsReplaceable("pale", "bae");
+
+            // Assert
+            Assert.IsFalse(res);
+        }
+    }
+}
--- StringReplacement/StringReplacement/Replacer.cs
+++ StringReplacement/StringReplacement/Replacer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StringReplacement
+﻿namespace StringReplacement
 {
     public class Replacer
     {
@@ -12,15 +6,15 @@ namespace StringReplacement
         {
             if(s1.Length == s2.Length)
             {
-                return ReplaceInPlace(s1, s2);
+                return OnePlaceEdit(s1, s2);
             }
             else if(s1.Length + 1 == s2.Length)
             {
-                return OnePlaceEdit(s1, s2);
+                return ReplaceInPlace(s1, s2);
             }
             else if (s1.Length -1 == s2.Length)
             {
-                return OnePlaceEdit(s2, s1);
+                return ReplaceInPlace(s2, s1);
             }
 
             return false;
@@ -28,12 +22,45 @@ namespace StringReplacement
 
         private bool OnePlaceEdit(string s1, string s2)
         {
-            
+            bool changed = false;
+
+            for(int i = 0; i < s1.Length; i++)
+            {
+                if(s1[i] != s2[i])
+                {
+                    if(changed)
+                    {
+                        return false;
+                    }
+                    changed = true;
+                }
+            }
+
+            return true;
         }
 
         private bool ReplaceInPlace(string s1, string s2)
         {
-            
+            var index1 = 0;
+            var index2 = 0;
+
+            while(index2 < s2.Length && index1 < s1.Length)
+            {
+                if(s1[index1] != s2[index2])
+                {
+                    if(index1 != index2)
+                    {
+                        return false;
+                    }
+                    index2++;
+                }
+                else
+                {
+                    index1++;
+                    index2++;
+                }
+            }
+            return true;
         }
     }
 }

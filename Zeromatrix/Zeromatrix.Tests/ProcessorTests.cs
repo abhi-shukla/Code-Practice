4b50f3d9fe9cef41a1a66bbd07fe95dcac7d0662
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-13 00:04:48 -0700

    Add Zero Matrix

---
+++ Zeromatrix/Zeromatrix.Tests/ProcessorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeromatrix;
+
+namespace Zeromatrix.Tests
+{
+    [TestClass]
+    public class ProcessorTests
+    {
+        [TestMethod]
+        public void ZeroIn_OnMatrixWithZeros_ShouldNullifyColumnsAndRows()
+        {
+            // Arrange
+            var matrix = new int[3, 3];
+            matrix[0, 0] = 0;
+            matrix[0, 1] = 1;
+            matrix[0, 2] = 2;
+            matrix[1, 0] = 7;
+            matrix[1, 1] = 8;
+            matrix[1, 2] = 3;
+            matrix[2, 0] = 6;
+            matrix[2, 1] = 5;
+            matrix[2, 2] = 4;
+            var processor = new Processor();
+
+            // Act
+            var res = processor.ZreoIn(matrix);
+
+            // Assert
+            
+        }
+    }
+}
---
+++ Zeromatrix/Zeromatrix/Processor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeromatrix
+{
+    public class Processor
+    {
+        public int[,] ZreoIn(int[,] matrix)
+        {
+            bool[] row = new bool[matrix.GetLength(0)];
+            bool[] column = new bool[matrix.GetLength(1)];
+
+            // Store the row and column which need to be nullfied
+            for (int i = 0; i < row.Length; i++)
+            {
+                for (int j = 0; j < column.Length; j++)
+                {
+                    if(matrix[i, j] == 0)
+                    {
+                        row[i] = true;
+                        column[j] = true;
+                    }
+                }
+            }
+
+            // Nullify Rows
+            for(int i = 0; i < row.Length; i++)
+            {
+                if(row[i])
+                {
+                    for(int j = 0; j < column.Length; j++)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            // Nullify Columns
+            for(int i = 0; i < column.Length; i++)
+            {
+                if(column[i])
+                {
+                    for(int j = 0; j < row.Length; j++)
+                    {
+                        matrix[j, i] = 0;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
